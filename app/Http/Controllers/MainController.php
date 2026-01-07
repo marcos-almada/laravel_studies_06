bcd058c74ad26d80a482c835278063717cca45ad
Marcos Almada <112503502+marcos-almada@users.noreply.github.com>
2026-01-07 16:44:14 -0400

    Model Conventions do Eloquent ORM

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\TesteModel;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
-        $products = Product::all()->toArray();
+        $result = TesteModel::all()->toArray();
         echo '<pre>';
-        print_r($products);
+        print_r($result);
         echo '</pre>';
     }
 }
---
+++ app/Models/TesteModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TesteModel extends Model
+{
+    // se quiser definir qual es a tabela do model
+    protected $table = 'products';
+
+    // definir a chave primaria
+    protected $primaryKey = 'id';
+
+    // se a coluna da chave primaria nao for auto increment
+    public $incrementing = true;
+
+    // se nao quiser as colunas created_at e updated_at
+    public $timestamps = false;
+
+    // se quiser alterar a data do created_at e updated_at
+    protected $dateFormat = 'Y-m-d H:i:s';
+}
