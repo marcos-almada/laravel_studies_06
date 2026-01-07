0bef2b778952b71f0e818b7513ba49ae76fdc690
Marcos Almada <112503502+marcos-almada@users.noreply.github.com>
2026-01-07 15:34:05 -0400

    Preparação do Projeto e introdução ao Eloquent ORM

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+Route::get('/teste', function () {
+
+    $products = Product::all();
+    echo '<pre>';
+    print_r($products->toArray());
+    echo '</pre>';
+
+
+});
